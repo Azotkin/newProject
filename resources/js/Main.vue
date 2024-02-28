984974cc392354fede5d66c2eb6fc0833164f4f7
Anatoly <anatoly@gmail.com>
2024-02-28 12:01:23 +0300

    init

---
+++ resources/js/Main.vue
@@ -0,0 +1,125 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg row">
+
+        <div class="col-md-9">
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                        Имя
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Фамилия
+                    </th>
+                    <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                        Отчество
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Возраст
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Пол
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Образование
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Город проживания
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr class="border-b border-gray-200 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                        Apple MacBook Pro 17"
+                    </th>
+                    <td class="px-6 py-4">
+                        Silver
+                    </td>
+                    <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                        Laptop
+                    </td>
+                    <td class="px-6 py-4">
+                        $2999
+                    </td>
+                </tr>
+                <tr class="border-b border-gray-200 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                        Microsoft Surface Pro
+                    </th>
+                    <td class="px-6 py-4">
+                        White
+                    </td>
+                    <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                        Laptop PC
+                    </td>
+                    <td class="px-6 py-4">
+                        $1999
+                    </td>
+                </tr>
+                <tr class="border-b border-gray-200 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                        Magic Mouse 2
+                    </th>
+                    <td class="px-6 py-4">
+                        Black
+                    </td>
+                    <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                        Accessories
+                    </td>
+                    <td class="px-6 py-4">
+                        $99
+                    </td>
+                </tr>
+                <tr class="border-b border-gray-200 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                        Google Pixel Phone
+                    </th>
+                    <td class="px-6 py-4">
+                        Gray
+                    </td>
+                    <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                        Phone
+                    </td>
+                    <td class="px-6 py-4">
+                        $799
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                        Apple Watch 5
+                    </th>
+                    <td class="px-6 py-4">
+                        Red
+                    </td>
+                    <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                        Wearables
+                    </td>
+                    <td class="px-6 py-4">
+                        $999
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="col-md-3">
+
+            <a href="#" class="col-md-12 row btn btn-primary  active" role="button" aria-pressed="true">Primary link</a>
+            <a href="#" class="col-md-12 row btn btn-primary  active" role="button" aria-pressed="true">Primary link</a>
+            <a href="#" class="col-md-12 row btn btn-primary  active" role="button" aria-pressed="true">Primary link</a>
+
+        </div>
+
+
+    </div>
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+import './bootstrap';
+import Main from "./Main.vue";
+import { createApp } from 'vue'
+
+
+createApp(Main).mount('#app')
